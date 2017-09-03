d37c46cbc48b4eb8882ded45e56bf72e81afa04b
Elliot Saba <staticfloat@gmail.com>
2017-09-03 10:52:21 -0700

    Update binary urls

--- deps/build.jl
+++ deps/build.jl
@@ -9,16 +9,18 @@ include(Pkg.dir("BinaryProvider", "src", "BinDepsIntegration.jl"))
 nettle = library_dependency("nettle", aliases = ["libnettle","libnettle-4-6","libnettle-6-1","libnettle-6-2"])
 
 # BinaryProvider support
-const prefix="https://github.com/staticfloat/NettleBuilder/releases/download/v3.3.2"
+const bin_prefix="https://github.com/staticfloat/NettleBuilder/releases/download/v3.3.3"
 const downloads = Dict(
-    "aarch64-linux-gnu" => ("$prefix/nettle.aarch64-linux-gnu.tar.gz", "34f3c9453a4c523b555f00f8fc17534f504f7d5e6e74360b77e88c88e2d76b1e"),
-    "powerpc64le-linux-gnu" => ("$prefix/nettle.powerpc64le-linux-gnu.tar.gz", "6bdbd9273d16e9c8a81755d0f164bb67dfc7ac97061f6ef83a4253dc56e8a363"),
-    "x86_64-linux-gnu" => ("$prefix/nettle.x86_64-linux-gnu.tar.gz", "cd031f560456f6ff66bc0b4d38235826116671f13c1ba78d90d7cc8fd91700e9"),
-    "x86_64-apple-darwin14" => ("$prefix/nettle.x86_64-apple-darwin14.tar.gz", "7bd084b2a619ca6b87c8cf3dee201c289ee5b0ee41da95d8417bdcdae6a27524"),
-    "x86_64-w64-mingw32" => ("$prefix/nettle.x86_64-w64-mingw32.tar.gz", "b2810b88da10dd02a47f5ec0b4b84bd943e046447b9b07899858aee4b6ce540f"),
-    "arm-linux-gnueabihf" => ("$prefix/nettle.arm-linux-gnueabihf.tar.gz", "1af16aa5d7701c4bccb3041673c051ecb6ca7cb429853b33297f51278df5618a"),
+    "linuxaarch64" => ("$bin_prefix/nettle.aarch64-linux-gnu.tar.gz", "271c4897754eb353df60fd7ebe167eea8aeffc677f8982c2ef4b4fb5eb636475"),
+    "linuxppc64le" => ("$bin_prefix/nettle.powerpc64le-linux-gnu.tar.gz", "e38189ed6283e08831e2d2284eb67c720661d2a30fe86189dc0ba89125334e28"),
+    "linuxarmv7l" => ("$bin_prefix/nettle.arm-linux-gnueabihf.tar.gz", "e4ca07b459a134bd397a5fe8bd0e0b89156bbe5277f6d65a128155bbca6c1119"),
+    "linux32" => ("$bin_prefix/nettle.i686-linux-gnu.tar.gz", "da842f30c32567c145b0cd550b15b602934e7664182b41bdc5256a8448d00932"),
+    "linux64" => ("$bin_prefix/nettle.x86_64-linux-gnu.tar.gz", "c614be2e4e7a3b9e5b531bab70533935e14308e0092a6d786a7aad4d0f599f7a"),
+    "mac64" => ("$bin_prefix/nettle.x86_64-apple-darwin14.tar.gz", "43ca1837baf37df895e5791d4da6c52030b47359b36c3b08bded041774b33ef8"),
+    "win32" => ("$bin_prefix/nettle.i686-w64-mingw32.tar.gz", "5893dd0be244c31e653c8076dc414d7f06fa27aece910d12f1e976fb19fcfb98"),
+    "win64" => ("$bin_prefix/nettle.x86_64-w64-mingw32.tar.gz", "9b47d73bf03e40c978f4757a7a67fce92a9c073deb68cf6b91ce2154ae5625c3"),
 )
-if platform_triplet(platform_key()) in keys(downloads)
+if platform_key() in keys(downloads)
     url, hash = downloads[platform_triplet(platform_key())]
     @BP_provides(url, hash, nettle)
 end
